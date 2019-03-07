6cc5c3eb722338c700d17bf6fbe874153da8b031
bartvbl <bartvblokl@gmail.com>
2019-03-07 15:03:58 +0100

    Avoided overflow in mesh merging

--- src/experiments/clutterBox/clutterBoxUtilities.cpp
+++ src/experiments/clutterBox/clutterBoxUtilities.cpp
@@ -19,17 +19,17 @@ DeviceMesh combineMeshesOnGPU(std::vector<DeviceMesh> meshes) {
 
     combinedMesh.vertexCount = totalVertexCount;
 
-    unsigned int pointerBaseIndex = 0;
+    size_t pointerBaseIndex = 0;
     for(unsigned int i = 0; i < meshes.size(); i++) {
         size_t meshBufferSize = meshes.at(i).vertexCount * sizeof(float);
 
-        cudaMemcpy(combinedMesh.normals_x + pointerBaseIndex, meshes.at(i).normals_x, meshBufferSize, cudaMemcpyDeviceToDevice);
-        cudaMemcpy(combinedMesh.normals_y + pointerBaseIndex, meshes.at(i).normals_y, meshBufferSize, cudaMemcpyDeviceToDevice);
-        cudaMemcpy(combinedMesh.normals_z + pointerBaseIndex, meshes.at(i).normals_z, meshBufferSize, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(&combinedMesh.normals_x[pointerBaseIndex], meshes.at(i).normals_x, meshBufferSize, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(&combinedMesh.normals_y[pointerBaseIndex], meshes.at(i).normals_y, meshBufferSize, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(&combinedMesh.normals_z[pointerBaseIndex], meshes.at(i).normals_z, meshBufferSize, cudaMemcpyDeviceToDevice);
 
-        cudaMemcpy(combinedMesh.vertices_x + pointerBaseIndex, meshes.at(i).vertices_x, meshBufferSize, cudaMemcpyDeviceToDevice);
-        cudaMemcpy(combinedMesh.vertices_y + pointerBaseIndex, meshes.at(i).vertices_y, meshBufferSize, cudaMemcpyDeviceToDevice);
-        cudaMemcpy(combinedMesh.vertices_z + pointerBaseIndex, meshes.at(i).vertices_z, meshBufferSize, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(&combinedMesh.vertices_x[pointerBaseIndex], meshes.at(i).vertices_x, meshBufferSize, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(&combinedMesh.vertices_y[pointerBaseIndex], meshes.at(i).vertices_y, meshBufferSize, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(&combinedMesh.vertices_z[pointerBaseIndex], meshes.at(i).vertices_z, meshBufferSize, cudaMemcpyDeviceToDevice);
 
         pointerBaseIndex += meshes.at(i).vertexCount;
     }
