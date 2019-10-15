d8ac59996ebf3ab58e312ade7c23468014c54c97
bartvbl <bartvblokl@gmail.com>
2019-10-15 14:48:59 +0200

    Fixed the server manager script not deleting the run deadline when removing a GPU from the pool

--- tools/servermanager/servermanager.py
+++ tools/servermanager/servermanager.py
@@ -15,7 +15,7 @@ from prompt_toolkit.completion import WordCompleter
 print("-- Server Manager v0.1 --")
 
 seedfile = "seeds_lotsofobjects.txt"
-outputDir = '../combinedoutput/output_qsifix_v4_lotsofobjects_si_supportangle180/'
+outputDir = '../combinedoutput/output_qsifix_v4_lotsofobjects_10_objects_only/'
 
 DAEMONONLY_timedstops = {}
 DAEMONONLY_currentSeedIndex = 0
@@ -53,7 +53,7 @@ def launchInstance(gpuID):
 
 	log("Launching job with ID " + str(DAEMONONLY_currentSeedIndex) + ' on GPU ' + str(gpuID))
 
-	cmd = subprocess.run(['nvidia-docker run -ti -d -v "/home/bartiver/SHREC17:/home/bartiver/SHREC17" --rm --device /dev/nvidia3:/dev/nvidia0 bartiver_qsiverification:latest ' + str(gpuID) + " " + str(DAEMONONLY_currentSeedIndex) + " " + str(DAEMONONLY_currentSeedIndex+1) + ' "' + seedfile + '" ' + '"--source-directory=/home/bartiver/SHREC17/ --object-counts=1,5,10 --descriptors=si --box-size=1 --spin-image-support-angle-degrees=180 --spin-image-width=0.3 --dump-raw-search-results --override-total-object-count=10"'], shell=True, stdout=subprocess.PIPE)
+	cmd = subprocess.run(['nvidia-docker run -ti -d -v "/home/bartiver/SHREC17:/home/bartiver/SHREC17" --rm --device /dev/nvidia3:/dev/nvidia0 bartiver_qsiverification:latest ' + str(gpuID) + " " + str(DAEMONONLY_currentSeedIndex) + " " + str(DAEMONONLY_currentSeedIndex+1) + ' "' + seedfile + '" ' + '"--source-directory=/home/bartiver/SHREC17/ --object-counts=10 --descriptors=si --box-size=1 --spin-image-support-angle-degrees=180 --spin-image-width=0.3 --dump-raw-search-results --override-total-object-count=10"'], shell=True, stdout=subprocess.PIPE)
 	subprocess.run(['docker rename ' + cmd.stdout.decode('ascii').strip() + ' bartiver_qsiverification_gpu' + ('0' if gpuID < 10 else '') + str(gpuID)], shell=True)
 
 	DAEMONONLY_activeseeds[gpuIndex] = DAEMONONLY_currentSeedIndex
@@ -132,6 +132,8 @@ def gpuDaemon():
 				elif job['command'] == 'remove':
 					gpuID = job['id']
 					if gpuID in DAEMONONLY_gpuIDs:
+						if str(gpuID) in DAEMONONLY_timedstops:
+							del DAEMONONLY_timedstops[str(gpuID)]
 						stopInstance(gpuID)
 						gpuIndex = DAEMONONLY_gpuIDs.index(gpuID)
 						processingSeed = DAEMONONLY_activeseeds[gpuIndex]
