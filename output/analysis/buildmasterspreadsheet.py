504ffadbf038657cdea1730dd66c13047b6f7fd9
bartvbl <bartvblokl@gmail.com>
2019-10-17 16:41:53 +0200

    First version of master spreadsheet script

--- output/analysis/buildmasterspreadsheet.py
+++ output/analysis/buildmasterspreadsheet.py
@@ -21,19 +21,26 @@ inputDirectoryDatasetNames = [
     'QSI, with early exit',
     'QSI, primary QSI IDUN run',
     'QSI, failed jobs from IDUN run',
-    'SI, 180 support angle, 5 objects',
-    'SI, 60 support angle, '
-
+    'SI, 60 support angle, primary IDUN run',
+    'SI, 60 support angle, secondary IDUN run'
 ]
 outfile = 'final_results/master_spreadsheet.xls'
 
 # Last known fault in code: unsigned integer subtraction in QSI comparison function
 # Date threshold corresponds to this commit
-qsiResultsValidAfter = datetime.datetime(year=2019, month=10, day=7, hour=15, minute=14, second=0, microsecond=0)
+def isQsiResultValid(fileCreationDateTime, resultJson):
+    qsiResultsValidAfter = datetime.datetime(year=2019, month=10, day=7, hour=15, minute=14, second=0, microsecond=0)
+    return fileCreationDateTime > qsiResultsValidAfter
 
 # Last known fault in code: override object count did not match
 # Date threshold corresponds to this commit
-siResultsValidAfter = datetime.datetime(year=2019, month=9, day=26, hour=17, minute=28, second=0, microsecond=0)
+def isSiResultValid(fileCreationDateTime, resultJson):
+    siResultsValidAfter = datetime.datetime(year=2019, month=9, day=26, hour=17, minute=28, second=0, microsecond=0)
+    hasCorrectOverrideObjectCount = 'overrideObjectCount' in resultJson and resultJson['overrideObjectCount'] == 10
+    hasCorrectSampleSetSize = resultJson['sampleSetSize'] == 10
+    hasValidCreationDateTime = siResultsValidAfter < fileCreationDateTime
+
+    return hasCorrectOverrideObjectCount or hasCorrectSampleSetSize
 
 # -- global initialisation --
 
@@ -66,48 +73,90 @@ def extractExperimentSettings(loadedJson):
 
 def loadOutputFileDirectory(path):
     originalFiles = os.listdir(path)
+    # Filter out the raw output file directories
+    originalFiles = [x for x in originalFiles if x != 'raw' and x != 'rawless']
+
     results = {
         'path': path,
-        'results': {},
+        'results': {'QSI': {}, 'SI': {}},
         'settings': {}
     }
 
-    ignored_count_si = 0
-    ignored_count_qsi = 0
+    jsonCache = {}
+
+    ignoredListQSI = []
+    ignoredListSI = []
+
+    allQSIResultsInvalid = False
+    allSIResultsInvalid = False
     for fileindex, file in enumerate(originalFiles):
-        if file == 'raw' or file == 'rawless':
-            continue
-        filename_creation_time_part = file.split('_')[0]
-        creation_time = datetime.datetime.strptime(filename_creation_time_part, "%Y-%m-%d %H-%M-%S")
-        if creation_time < qsiResultsValidAfter:
-            ignored_count_qsi += 1
-        if creation_time < siResultsValidAfter:
-            ignored_count_si += 1
+        print(str(fileindex+1) + '/' + str(len(originalFiles)), file + '        ', end='\r')
+        with open(os.path.join(path, file), 'r') as openFile:
+            # Read JSON file
+            fileContents = json.loads(openFile.read())
+            jsonCache[file] = fileContents
 
-    if ignored_count_qsi > 0:
-        print('\t%i/%i QSI images were created before the threshold deadline. QSI images will be ignored from this dataset.' % (ignored_count_qsi, len(originalFiles)))
-    if ignored_count_si > 0:
-        print('\t%i/%i SI images were created before the threshold deadline. SI images will be ignored from this dataset.' % (ignored_count_si, len(originalFiles)))
-
+            # Check validity of code by using knowledge about previous code changes
+            filename_creation_time_part = file.split('_')[0]
+            creation_time = datetime.datetime.strptime(filename_creation_time_part, "%Y-%m-%d %H-%M-%S")
+            if not isQsiResultValid(creation_time, fileContents):
+                ignoredListQSI.append(file)
+                allQSIResultsInvalid = True
+            if not isSiResultValid(creation_time, fileContents):
+                ignoredListSI.append(file)
+                allSIResultsInvalid = True
 
     previousExperimentSettings = None
     for fileindex, file in enumerate(originalFiles):
         print(str(fileindex+1) + '/' + str(len(originalFiles)), file + '        ', end='\r')
-        if file == 'raw' or file == 'rawless':
-            continue
         with open(os.path.join(path, file), 'r') as openFile:
-            fileContents = json.loads(openFile.read())
+            # Read JSON file
+            fileContents = jsonCache[file]
+
+            # Check if settings are the same as other files in the folder
             currentExperimentSettings = extractExperimentSettings(fileContents)
             if previousExperimentSettings is not None:
                 if currentExperimentSettings != previousExperimentSettings:
+                    # Any discrepancy here is a fatal exception. It NEEDS attention regardless
                     raise Exception("Experiment settings mismatch in the same batch! File: " + file)
-            results['results'][fileContents['seed']] = fileContents
             previousExperimentSettings = currentExperimentSettings
+            results['settings'] = currentExperimentSettings
+
+            # Check for other incorrect settings. Ignore files if detected
+            if 0 in fileContents['imageCounts']:
+                if file not in ignoredListQSI:
+                    ignoredListQSI.append(file)
+                if file not in ignoredListSI:
+                    ignoredListSI.append(file)
+                #print('ignored 0',file)
+            if fileContents['spinImageWidthPixels'] == 32:
+                if file not in ignoredListQSI:
+                    ignoredListQSI.append(file)
+                if file not in ignoredListSI:
+                    ignoredListSI.append(file)
+
+            # Beauty checks
+            if file not in ignoredListQSI and allQSIResultsInvalid:
+                ignoredListQSI.append(file)
+            if file not in ignoredListSI and allSIResultsInvalid:
+                ignoredListSI.append(file)
+
+            containsQSIResults = ('descriptors' in fileContents and 'qsi' in fileContents['descriptors']) or 'QSIhistograms' in fileContents
+            containsSIResults = ('descriptors' in fileContents and 'si' in fileContents['descriptors']) or 'SIhistograms' in fileContents
+
+            # Sanity checks are done. We can now add any remaining valid entries to the result lists
+            if not file in ignoredListQSI and not allQSIResultsInvalid and containsQSIResults:
+                results['results']['QSI'][str(fileContents['seed'])] = fileContents
+            if not file in ignoredListSI and not allSIResultsInvalid and containsSIResults:
+                results['results']['SI'][str(fileContents['seed'])] = fileContents
+
     print()
+    print('%i/%i QSI files had discrepancies and had to be ignored.' % (len(ignoredListQSI), len(originalFiles)))
+    print('%i/%i SI files had discrepancies and had to be ignored.' % (len(ignoredListSI), len(originalFiles)))
 
     results['settings'] = previousExperimentSettings
     pp = pprint.PrettyPrinter(indent=4)
-    print(pp.pformat(results['settings']))
+    #print(pp.pformat(results['settings']))
 
     return results
 
@@ -125,6 +174,18 @@ for directory in inputDirectories:
 
 
 print('Processing..')
+seedSet = set()
+for directory in inputDirectories:
+    for seed in loadedResults[directory]['results']['QSI'].keys():
+        seedSet.add(seed)
+    for seed in loadedResults[directory]['results']['SI'].keys():
+        seedSet.add(seed)
+seedList = [x for x in seedSet]
+
+print('\tFound', len(seedSet), 'seeds in result sets')
+
+
+
 
 # -- Dump to spreadsheet --
 
@@ -134,14 +195,87 @@ book = xlwt.Workbook(encoding="utf-8")
 
 # 1. Create data page for dataset settings table
 experimentSheet = book.add_sheet("Experiment Overview")
-
-
+for directoryIndex, directory in enumerate(inputDirectories):
+    directoryName = inputDirectoryDatasetNames[directoryIndex]
+    experimentSheet.write(directoryIndex + 1, 0, directoryName)
+    resultSet = loadedResults[directory]
+    for keyIndex, key in enumerate(resultSet['settings'].keys()):
+        if directoryIndex == 0:
+            experimentSheet.write(0, keyIndex + 1, str(key))
+        experimentSheet.write(directoryIndex + 1, keyIndex + 1, str(resultSet['settings'][key]))
 
 # 2. Create data page for mapping of seed -> % at rank 0
-top0sheet = book.add_sheet("Rank 0 results")
+top0sheetQSI = book.add_sheet("Rank 0 QSI results")
+top0sheetSI = book.add_sheet("Rank 0 SI results")
+top10sheetQSI = book.add_sheet("Top 10 QSI results")
+top10sheetSI = book.add_sheet("Top 10 SI results")
+currentColumn = 1
+top0sheetQSI.write(0, 0, 'seed')
+top0sheetSI.write(0, 0, 'seed')
+top10sheetQSI.write(0, 0, 'seed')
+top10sheetSI.write(0, 0, 'seed')
+vertexCountSheet = book.add_sheet("Vertex Count Sanity Check")
 
-# 3. Create data page for mapping of seed -> % at rank 0 - 9
-top10sheet = book.add_sheet("Top 10 results")
+for directoryIndex, directory in enumerate(inputDirectories):
+    if directoryIndex == 0:
+        for seedIndex, seed in enumerate(seedList):
+            top0sheetQSI.write(seedIndex + 1, 0, str(seed))
+            top0sheetSI.write(seedIndex + 1, 0, str(seed))
+    resultSet = loadedResults[directory]
+    directoryName = inputDirectoryDatasetNames[directoryIndex]
+    for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
+        top0sheetQSI.write(0, currentColumn + sampleCountIndex,
+                           directoryName + ' (' + str(sampleObjectCount) + ' ' + objects(
+                               len(resultSet['settings']['sampleObjectCounts'])) + ')')
+        top0sheetSI.write(0, currentColumn + sampleCountIndex,
+                          directoryName + ' (' + str(sampleObjectCount) + ' ' + objects(
+                              len(resultSet['settings']['sampleObjectCounts'])) + ')')
+        top10sheetQSI.write(0, currentColumn + sampleCountIndex,
+                           directoryName + ' (' + str(sampleObjectCount) + ' ' + objects(
+                               len(resultSet['settings']['sampleObjectCounts'])) + ')')
+        top10sheetSI.write(0, currentColumn + sampleCountIndex,
+                          directoryName + ' (' + str(sampleObjectCount) + ' ' + objects(
+                              len(resultSet['settings']['sampleObjectCounts'])) + ')')
+    for seedIndex, seed in enumerate(seedList):
+        if seed in resultSet['results']['QSI']:
+            for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
+                entry = resultSet['results']['QSI'][seed]
+                experimentIterationCount = len(resultSet['settings']['sampleObjectCounts'])
+                totalImageCount = entry['imageCounts'][0]
+                percentageAtPlace0 = float(entry['QSIhistograms'][str(sampleCountIndex)]['0']) / float(totalImageCount)
+                top0sheetQSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentageAtPlace0))
+
+                top10ImageCount = sum(entry['imageCounts'][0:min(10, len(entry['imageCounts']))])
+                totalImageCountInTop10 = sum(
+                    [entry['QSIhistograms'][str(sampleCountIndex)][str(x)] for x in range(0, 10) if
+                    str(x) in entry['QSIhistograms'][str(sampleCountIndex)]])
+                percentInTop10 = float(totalImageCountInTop10) / float(top10ImageCount)
+                top10sheetQSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentInTop10))
+
+                vertexCountSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, entry['imageCounts'][0])
+        if seed in resultSet['results']['SI']:
+            for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
+                entry = resultSet['results']['SI'][seed]
+                experimentIterationCount = len(resultSet['settings']['sampleObjectCounts'])
+                totalImageCount = entry['imageCounts'][0]
+                percentageAtPlace0 = float(entry['SIhistograms'][str(sampleCountIndex)]['0']) / float(totalImageCount)
+                top0sheetSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentageAtPlace0))
+
+                top10ImageCount = sum(entry['imageCounts'][0:min(10, len(entry['imageCounts']))])
+                totalImageCountInTop10 = sum(
+                    [entry['SIhistograms'][str(sampleCountIndex)][str(x)] for x in range(0, 10) if
+                    str(x) in entry['SIhistograms'][str(sampleCountIndex)]])
+                percentInTop10 = float(totalImageCountInTop10) / float(top10ImageCount)
+                top10sheetSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentInTop10))
+
+                vertexCountSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, entry['imageCounts'][0])
+    currentColumn += len(resultSet['settings']['sampleObjectCounts'])
 
 
+
+
+
+
+
+book.save(outfile)
 print('Complete.')
