092cf61012049aec84bb9b8653698e8e2d514efb
bartvbl <bartvblokl@gmail.com>
2019-10-21 17:42:36 +0200

    Added sheets for timings, fixes, beautifications

--- output/analysis/buildmasterspreadsheet.py
+++ output/analysis/buildmasterspreadsheet.py
@@ -8,28 +8,19 @@ from math import sqrt
 
 # -- settings --
 
-inputDirectories = [
-    '../HEIDRUNS/output_qsifix_v4_noearlyexit/output',
-    '../HEIDRUNS/output_qsifix_v4_withearlyexit/output',
-    '../IDUNRUNS/output_lotsofobjects_v4',
-    '../HEIDRUNS/output_qsifix_v4_lotsofobjects_idun_failed/output',
-    '../IDUNRUNS/output_smallsupportangle_lotsofobjects',
-    '../IDUNRUNS/output_qsifix_smallsupportangle_rerun',
-    '../HEIDRUNS/output_qsifix_v4_lotsofobjects_10_objects_only/output',
-    '../IDUNRUNS/output_mainchart_si_v4_15',
-    '../IDUNRUNS/output_mainchart_si_v4_10',
-]
-inputDirectoryDatasetNames = [
-    'QSI\nno early exit',
-    'QSI\nwith early exit',
-    'QSI\nprimary QSI IDUN run',
-    'QSI\nfailed jobs from IDUN run',
-    'SI\n60 support angle\nprimary IDUN run',
-    'SI\n60 support angle\nsecondary IDUN run',
-    'SI\n180 support angle\nHEID',
-    'SI\n180 support angle\nIDUN',
-    'SI\n180 support angle\nIDUN',
-]
+inputDirectories = {
+    '../HEIDRUNS/output_qsifix_v4_noearlyexit/output': 'No early exit',
+    '../HEIDRUNS/output_qsifix_v4_withearlyexit/output': 'Early exit',
+    '../HEIDRUNS/output_qsifix_v4_lotsofobjects_idun_failed/output': 'Failed jobs from IDUN run',
+    '../IDUNRUNS/output_lotsofobjects_v4': 'primary QSI IDUN run',
+
+    '../HEIDRUNS/output_qsifix_v4_lotsofobjects_10_objects_only/output': '180 support angle, 10 objects',
+    '../HEIDRUNS/output_qsifix_v4_lotsofobjects_5_objects_only/output': '180 support angle, 5 objects',
+    '../IDUNRUNS/output_smallsupportangle_lotsofobjects': '60 support angle, primary',
+    '../IDUNRUNS/output_qsifix_smallsupportangle_rerun': '60 support angle, secondary',
+    '../IDUNRUNS/output_mainchart_si_v4_15': '180 support angle, 1 & 5 objects',
+    '../IDUNRUNS/output_mainchart_si_v4_10': '180 support angle, 10 objects',
+}
 outfile = 'final_results/master_spreadsheet.xls'
 
 # Last known fault in code: unsigned integer subtraction in QSI comparison function
@@ -99,7 +90,12 @@ def loadOutputFileDirectory(path):
         print(str(fileindex+1) + '/' + str(len(originalFiles)), file + '        ', end='\r')
         with open(os.path.join(path, file), 'r') as openFile:
             # Read JSON file
-            fileContents = json.loads(openFile.read())
+            try:
+                fileContents = json.loads(openFile.read())
+            except Exception as e:
+                print('FAILED TO READ FILE: ' + str(file))
+                print(e)
+                continue
             jsonCache[file] = fileContents
 
             # Check validity of code by using knowledge about previous code changes
@@ -174,14 +170,14 @@ def objects(count):
 
 print('Loading original files..')
 loadedResults = {}
-for directory in inputDirectories:
+for directory in inputDirectories.keys():
     print('Loading directory:', directory)
     loadedResults[directory] = loadOutputFileDirectory(directory)
 
 
 print('Processing..')
 seedSet = set()
-for directory in inputDirectories:
+for directory in inputDirectories.keys():
     for seed in loadedResults[directory]['results']['QSI'].keys():
         seedSet.add(seed)
     for seed in loadedResults[directory]['results']['SI'].keys():
@@ -199,39 +195,68 @@ print('Dumping spreadsheet..')
 
 book = xlwt.Workbook(encoding="utf-8")
 
-# 1. Create data page for dataset settings table
+# Create data page for dataset settings table
 experimentSheet = book.add_sheet("Experiment Overview")
-for directoryIndex, directory in enumerate(inputDirectories):
-    directoryName = inputDirectoryDatasetNames[directoryIndex]
+allColumns = set()
+for directoryIndex, directory in enumerate(inputDirectories.keys()):
+    result = loadedResults[directory]
+    allColumns = allColumns.union(set(result['settings']))
+
+for directoryIndex, directory in enumerate(inputDirectories.keys()):
+    directoryName = inputDirectories[directory]
     experimentSheet.write(directoryIndex + 1, 0, directoryName)
-    resultSet = loadedResults[directory]
-    for keyIndex, key in enumerate(resultSet['settings'].keys()):
+    result = loadedResults[directory]
+    for keyIndex, key in enumerate(allColumns):
         if directoryIndex == 0:
             experimentSheet.write(0, keyIndex + 1, str(key))
-        experimentSheet.write(directoryIndex + 1, keyIndex + 1, str(resultSet['settings'][key]))
+        if key in result['settings']:
+            experimentSheet.write(directoryIndex + 1, keyIndex + 1, str(result['settings'][key]))
+        else:
+            experimentSheet.write(directoryIndex + 1, keyIndex + 1, ' ')
 
-# 2. Create data page for mapping of seed -> % at rank 0
+# Sheets
 top0sheetQSI = book.add_sheet("Rank 0 QSI results")
 top0sheetSI = book.add_sheet("Rank 0 SI results")
 top10sheetQSI = book.add_sheet("Top 10 QSI results")
 top10sheetSI = book.add_sheet("Top 10 SI results")
-currentColumn = 1
-top0sheetQSI.write(0, 0, 'seed')
-top0sheetSI.write(0, 0, 'seed')
-top10sheetQSI.write(0, 0, 'seed')
-top10sheetSI.write(0, 0, 'seed')
+qsiGenerationSpeedSheet = book.add_sheet("QSI Generation Times")
+siGenerationSpeedSheet = book.add_sheet("SI Generation Times")
+qsiComparisonSpeedSheet = book.add_sheet("QSI Comparison Times")
+siComparisonSpeedSheet = book.add_sheet("SI Comparison Times")
 vertexCountSheet = book.add_sheet("Vertex Count Sanity Check")
 
-for directoryIndex, directory in enumerate(inputDirectories):
+# seed column is 0, data starts at column 1
+currentColumn = 1
+
+for directoryIndex, directory in enumerate(inputDirectories.keys()):
+
+    # Writing seed column
     if directoryIndex == 0:
+        top0sheetQSI.write(0, 0, 'seed')
+        top0sheetSI.write(0, 0, 'seed')
+        top10sheetQSI.write(0, 0, 'seed')
+        top10sheetSI.write(0, 0, 'seed')
+        qsiGenerationSpeedSheet.write(0, 0, 'seed')
+        siGenerationSpeedSheet.write(0, 0, 'seed')
+        qsiComparisonSpeedSheet.write(0, 0, 'seed')
+        siComparisonSpeedSheet.write(0, 0, 'seed')
+        vertexCountSheet.write(0, 0, 'seed')
+
         for seedIndex, seed in enumerate(seedList):
             top0sheetQSI.write(seedIndex + 1, 0, str(seed))
             top0sheetSI.write(seedIndex + 1, 0, str(seed))
             top10sheetQSI.write(seedIndex + 1, 0, str(seed))
             top10sheetSI.write(seedIndex + 1, 0, str(seed))
+            qsiGenerationSpeedSheet.write(seedIndex + 1, 0, str(seed))
+            siGenerationSpeedSheet.write(seedIndex + 1, 0, str(seed))
+            qsiComparisonSpeedSheet.write(seedIndex + 1, 0, str(seed))
+            siComparisonSpeedSheet.write(seedIndex + 1, 0, str(seed))
             vertexCountSheet.write(seedIndex + 1, 0, str(seed))
+
     resultSet = loadedResults[directory]
-    directoryName = inputDirectoryDatasetNames[directoryIndex]
+    directoryName = inputDirectories[directory]
+
+    # Writing column headers
     for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
         columnHeader = directoryName + ' (' + str(sampleObjectCount) + ' ' + objects(
                                len(resultSet['settings']['sampleObjectCounts'])) + ')'
@@ -239,41 +264,98 @@ for directoryIndex, directory in enumerate(inputDirectories):
         top0sheetSI.write(0, currentColumn + sampleCountIndex, columnHeader)
         top10sheetQSI.write(0, currentColumn + sampleCountIndex, columnHeader)
         top10sheetSI.write(0, currentColumn + sampleCountIndex, columnHeader)
+
+        qsiGenerationSpeedSheet.write(0, currentColumn + sampleCountIndex, columnHeader)
+        siGenerationSpeedSheet.write(0, currentColumn + sampleCountIndex, columnHeader)
+        qsiComparisonSpeedSheet.write(0, currentColumn + sampleCountIndex, columnHeader)
+        siComparisonSpeedSheet.write(0, currentColumn + sampleCountIndex, columnHeader)
+
         vertexCountSheet.write(0, currentColumn + sampleCountIndex, columnHeader)
+
+
     for seedIndex, seed in enumerate(seedList):
         if seed in resultSet['results']['QSI']:
             for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
+                # Top 1 performance
                 entry = resultSet['results']['QSI'][seed]
-                experimentIterationCount = len(resultSet['settings']['sampleObjectCounts'])
                 totalImageCount = entry['imageCounts'][0]
+                experimentIterationCount = len(resultSet['settings']['sampleObjectCounts'])
                 percentageAtPlace0 = float(entry['QSIhistograms'][str(sampleCountIndex)]['0']) / float(totalImageCount)
                 top0sheetQSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentageAtPlace0))
 
-                top10ImageCount = sum(entry['imageCounts'][0:min(10, len(entry['imageCounts']))])
+                # Top 10 performance
                 totalImageCountInTop10 = sum(
                     [entry['QSIhistograms'][str(sampleCountIndex)][str(x)] for x in range(0, 10) if
                     str(x) in entry['QSIhistograms'][str(sampleCountIndex)]])
-                percentInTop10 = float(totalImageCountInTop10) / float(top10ImageCount)
+                percentInTop10 = float(totalImageCountInTop10) / float(totalImageCount)
                 top10sheetQSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentInTop10))
 
+                # generation execution time
+                generationTime = entry['runtimes']['QSISampleGeneration']['total'][sampleCountIndex]
+                qsiGenerationSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, str(generationTime))
+
+                # search execution time
+                comparisonTime = entry['runtimes']['QSISearch']['total'][sampleCountIndex]
+                qsiComparisonSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, str(comparisonTime))
+
+                # Vertex count sanity check
                 vertexCountSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, entry['imageCounts'][0])
+        else:
+            for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
+                top0sheetQSI.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+                top10sheetQSI.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+                qsiGenerationSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+                qsiComparisonSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+
         if seed in resultSet['results']['SI']:
             for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
+                # Top 1 performance
                 entry = resultSet['results']['SI'][seed]
-                experimentIterationCount = len(resultSet['settings']['sampleObjectCounts'])
                 totalImageCount = entry['imageCounts'][0]
+                experimentIterationCount = len(resultSet['settings']['sampleObjectCounts'])
                 percentageAtPlace0 = float(entry['SIhistograms'][str(sampleCountIndex)]['0']) / float(totalImageCount)
                 top0sheetSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentageAtPlace0))
 
-                top10ImageCount = sum(entry['imageCounts'][0:min(10, len(entry['imageCounts']))])
+                # Top 10 performance
                 totalImageCountInTop10 = sum(
                     [entry['SIhistograms'][str(sampleCountIndex)][str(x)] for x in range(0, 10) if
                     str(x) in entry['SIhistograms'][str(sampleCountIndex)]])
-                percentInTop10 = float(totalImageCountInTop10) / float(top10ImageCount)
+                percentInTop10 = float(totalImageCountInTop10) / float(totalImageCount)
                 top10sheetSI.write(seedIndex + 1, currentColumn + sampleCountIndex, str(percentInTop10))
 
+                # generation execution time
+                generationTime = entry['runtimes']['SISampleGeneration']['total'][sampleCountIndex]
+                siGenerationSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, str(generationTime))
+
+                # search execution time
+                comparisonTime = entry['runtimes']['SISearch']['total'][sampleCountIndex]
+                siComparisonSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, str(comparisonTime))
+
+                # Vertex count sanity check
                 vertexCountSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, entry['imageCounts'][0])
+        else:
+            for sampleCountIndex, sampleObjectCount in enumerate(resultSet['settings']['sampleObjectCounts']):
+                top0sheetSI.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+                top10sheetSI.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+                siGenerationSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+                siComparisonSpeedSheet.write(seedIndex + 1, currentColumn + sampleCountIndex, ' ')
+
+    # Moving on to the next column
     currentColumn += len(resultSet['settings']['sampleObjectCounts'])
+
+# beauty addition.. Cuts off final column
+for seedIndex, seed in enumerate(seedList + ['dummy entry for final row']):
+    top0sheetQSI.write(seedIndex, currentColumn, ' ')
+    top10sheetQSI.write(seedIndex, currentColumn, ' ')
+    qsiGenerationSpeedSheet.write(seedIndex, currentColumn, ' ')
+    qsiComparisonSpeedSheet.write(seedIndex, currentColumn, ' ')
+
+    top0sheetSI.write(seedIndex, currentColumn, ' ')
+    top10sheetSI.write(seedIndex, currentColumn, ' ')
+    siGenerationSpeedSheet.write(seedIndex, currentColumn, ' ')
+    siComparisonSpeedSheet.write(seedIndex, currentColumn, ' ')
+
+    vertexCountSheet.write(seedIndex, currentColumn, ' ')
 
 
 
