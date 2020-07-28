37ec601eb567099542baa78633228dd5ce0a9b45
bartvbl <bartvblokl@gmail.com>
2020-07-28 17:31:14 +0200

    Implemented FPFH method adapter

---
+++ src/clutterbox/methods/FPFHMethod.cpp
@@ -0,0 +1,56 @@
+#include "FPFHMethod.h"
+#include <spinImage/gpu/fastPointFeatureHistogramGenerator.cuh>
+#include <spinImage/gpu/fastPointFeatureHistogramSearcher.cuh>
+
+SpinImage::array<char> FPFHMethod::generateDescriptors(
+        SpinImage::gpu::Mesh device_sceneAsMesh,
+        SpinImage::gpu::PointCloud device_sceneAsPointCloud,
+        SpinImage::array<SpinImage::gpu::DeviceOrientedPoint> device_descriptorOrigins,
+        Clutterbox::GenerationParameters parameters,
+        ExecutionTimes *executionTimes) {
+
+    SpinImage::debug::FPFHExecutionTimes fpfhExecutionTimes{};
+
+    SpinImage::array<SpinImage::gpu::FPFHDescriptor> descriptors = SpinImage::gpu::generateFPFHHistograms(
+            device_sceneAsPointCloud,
+            device_descriptorOrigins,
+            parameters.supportRadius,
+            &fpfhExecutionTimes);
+
+    executionTimes->append("total", fpfhExecutionTimes.totalExecutionTimeSeconds);
+    executionTimes->append("reformat", fpfhExecutionTimes.originReformatExecutionTimeSeconds);
+    executionTimes->append("spfh_origins", fpfhExecutionTimes.originSPFHGenerationExecutionTimeSeconds);
+    executionTimes->append("spfh_pointCloud", fpfhExecutionTimes.pointCloudSPFHGenerationExecutionTimeSeconds);
+    executionTimes->append("generation", fpfhExecutionTimes.fpfhGenerationExecutionTimeSeconds);
+
+    return {descriptors.length, reinterpret_cast<char*>(descriptors.content)};
+}
+
+SpinImage::array<unsigned int> FPFHMethod::computeSearchResultRanks(
+        SpinImage::array<char> device_needleDescriptors,
+        SpinImage::array<char> device_haystackDescriptors,
+        Clutterbox::SearchParameters parameters,
+        ExecutionTimes *executionTimes) {
+
+    SpinImage::debug::FPFHSearchExecutionTimes times{};
+
+    SpinImage::array<unsigned int> searchResultIndices = SpinImage::gpu::computeFPFHSearchResultRanks(
+            {device_needleDescriptors.length,
+             reinterpret_cast<SpinImage::gpu::FPFHDescriptor*>(device_needleDescriptors.content)},
+            {device_haystackDescriptors.length,
+             reinterpret_cast<SpinImage::gpu::FPFHDescriptor*>(device_haystackDescriptors.content)},
+             &times);
+
+    executionTimes->append("total", times.totalExecutionTimeSeconds);
+    executionTimes->append("search", times.searchExecutionTimeSeconds);
+
+    return searchResultIndices;
+}
+
+const std::string FPFHMethod::getMethodCommandLineParameterName() {
+    return "fpfh";
+}
+
+const std::string FPFHMethod::getMethodDumpFileName() {
+    return "FPFH";
+}
---
+++ src/clutterbox/methods/FPFHMethod.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <clutterbox/methods/types/ClutterboxMethod.h>
+
+class FPFHMethod : Clutterbox::Method {
+    float supportRadius;
+
+    SpinImage::array<char>
+    generateDescriptors(SpinImage::gpu::Mesh device_sceneAsMesh,
+                        SpinImage::gpu::PointCloud device_sceneAsPointCloud,
+                        SpinImage::array<SpinImage::gpu::DeviceOrientedPoint> device_origins,
+                        Clutterbox::GenerationParameters parameters,
+                        ExecutionTimes *executionTimes) override;
+
+    SpinImage::array<unsigned int> computeSearchResultRanks(SpinImage::array<char> device_needleDescriptors,
+                                                            SpinImage::array<char> device_haystackDescriptors,
+                                                            Clutterbox::SearchParameters parameters,
+                                                            ExecutionTimes *executionTimes) override;
+
+    const std::string getMethodCommandLineParameterName() override;
+
+    const std::string getMethodDumpFileName() override;
+};
