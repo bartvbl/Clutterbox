606a2847073dc934305209fabc47acb2144d4990
bartvbl <bartvblokl@gmail.com>
2020-07-28 20:30:21 +0200

    Added a convenience feature that allows querying execution times by name

--- src/clutterbox/methods/types/ExecutionTimes.h
+++ src/clutterbox/methods/types/ExecutionTimes.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <map>
 
 class ExecutionTimeMeasurement {
     std::string name;
@@ -14,9 +15,15 @@ public:
 
 class ExecutionTimes {
     std::vector<ExecutionTimeMeasurement> measurements;
+    std::map<std::string, double> measurementMap;
 
 public:
     void append(const std::string& name, double timeInSeconds) {
         measurements.emplace_back(name, timeInSeconds);
+        measurementMap.at(name) = timeInSeconds;
+    }
+
+    double getMeasurementByName(std::string name) {
+        return measurementMap.at(name);
     }
 };
