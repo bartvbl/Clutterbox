a3738dd434e7af492c92acd0fcaa200f6ba3b593
bartvbl <bartvblokl@gmail.com>
2020-07-28 19:45:57 +0200

    Implemented SPin Image method adapter

---
+++ src/clutterbox/methods/SIMethod.cpp
@@ -0,0 +1,56 @@
+#include <spinImage/gpu/spinImageGenerator.cuh>
+#include <spinImage/gpu/spinImageSearcher.cuh>
+#include "SIMethod.h"
+
+SpinImage::array<char> SIMethod::generateDescriptors(
+        SpinImage::gpu::Mesh device_sceneAsMesh,
+        SpinImage::gpu::PointCloud device_sceneAsPointCloud,
+        SpinImage::array<SpinImage::gpu::DeviceOrientedPoint> device_descriptorOrigins,
+        Clutterbox::GenerationParameters parameters,
+        ExecutionTimes *executionTimes) {
+
+    SpinImage::debug::SIExecutionTimes siExecutionTimes{};
+
+    SpinImage::array<SpinImage::gpu::SpinImageDescriptor> descriptors = SpinImage::gpu::generateSpinImages(
+            device_sceneAsPointCloud,
+            device_descriptorOrigins,
+            parameters.supportRadius,
+            supportAngle,
+            &siExecutionTimes);
+
+    executionTimes->append("total", siExecutionTimes.totalExecutionTimeSeconds);
+    executionTimes->append("initialisation", siExecutionTimes.initialisationTimeSeconds);
+    executionTimes->append("generation", siExecutionTimes.generationTimeSeconds);
+
+    return {descriptors.length, reinterpret_cast<char*>(descriptors.content)};
+}
+
+SpinImage::array<unsigned int> SIMethod::computeSearchResultRanks(
+        SpinImage::array<char> device_needleDescriptors,
+        SpinImage::array<char> device_haystackDescriptors,
+        Clutterbox::SearchParameters parameters,
+        ExecutionTimes *executionTimes) {
+
+    SpinImage::debug::SISearchExecutionTimes times{};
+
+    SpinImage::array<unsigned int> searchResultIndices = SpinImage::gpu::computeSpinImageSearchResultRanks(
+            {device_needleDescriptors.length,
+             reinterpret_cast<SpinImage::gpu::SpinImageDescriptor*>(device_needleDescriptors.content)},
+            {device_haystackDescriptors.length,
+             reinterpret_cast<SpinImage::gpu::SpinImageDescriptor*>(device_haystackDescriptors.content)},
+             &times);
+
+    executionTimes->append("total", times.totalExecutionTimeSeconds);
+    executionTimes->append("averaging", times.averagingExecutionTimeSeconds);
+    executionTimes->append("search", times.searchExecutionTimeSeconds);
+
+    return searchResultIndices;
+}
+
+const std::string SIMethod::getMethodCommandLineParameterName() {
+    return "si";
+}
+
+const std::string SIMethod::getMethodDumpFileName() {
+    return "SI";
+}
---
+++ src/clutterbox/methods/SIMethod.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <clutterbox/methods/types/ClutterboxMethod.h>
+
+class SIMethod : Clutterbox::Method {
+    float supportAngle;
+
+    SpinImage::array<char>
+    generateDescriptors(SpinImage::gpu::Mesh device_sceneAsMesh,
+                        SpinImage::gpu::PointCloud device_sceneAsPointCloud,
+                        SpinImage::array<SpinImage::gpu::DeviceOrientedPoint> device_origins,
+                        Clutterbox::GenerationParameters parameters,
+                        ExecutionTimes *executionTimes) override;
+
+    SpinImage::array<unsigned int> computeSearchResultRanks(SpinImage::array<char> device_needleDescriptors,
+                                                            SpinImage::array<char> device_haystackDescriptors,
+                                                            Clutterbox::SearchParameters parameters,
+                                                            ExecutionTimes *executionTimes) override;
+
+    const std::string getMethodCommandLineParameterName() override;
+
+    const std::string getMethodDumpFileName() override;
+
+    explicit SIMethod(float supportAngle) : supportAngle(supportAngle) {}
+};
